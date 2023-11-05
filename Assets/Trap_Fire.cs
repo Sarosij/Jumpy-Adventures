2e8046f394f2239c12e3bb04a2d43c1e15372fa0
Sarosij Ghosh Ray <rikray286@gmail.com>
2023-11-05 23:33:16 +0530

    Traps added

---
+++ Assets/Trap_Fire.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap_Fire : Trap
+{
+    public bool isWorking;
+    private Animator anim;
+    public float repeatRate;
+    public bool hasSwitcher;
+
+    private void Start() {
+        anim = GetComponent<Animator>();
+
+        if (!hasSwitcher)
+            InvokeRepeating ("FireSwitch", 0, repeatRate);
+    }
+
+    private void Update() 
+    {
+        anim.SetBool("isWorking", isWorking);
+    }
+
+    public void FireSwitch()
+    {
+        isWorking = !isWorking;
+    }
+
+    public void FireSwitchAfter(float seconds)
+    {
+        FireSwitch();
+        Invoke ("FireSwitch",seconds);
+    }
+         
+    protected override void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isWorking)
+            base.OnTriggerEnter2D(collision);
+    }
+}
